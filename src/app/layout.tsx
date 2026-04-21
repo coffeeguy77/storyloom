b93587cf96441e97042476a3dd5815f6599fe660
coffeeguy77 <info@beanculture.com.au>
2026-04-22 00:36:11 +1000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { SupabaseProvider } from '@/lib/useSupabase'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,10 +15,12 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-          <html lang="en">
-                <body className={inter.className}>
-                  {children}
-                </body>
-          </html>
-        )
+        <html lang="en">
+            <body className={inter.className}>
+                <SupabaseProvider>
+                    {children}
+                </SupabaseProvider>
+            </body>
+        </html>
+    )
 }
